2c4f033b1645570eac7e5076ba68983f490b51e2
g0djan <g0dj4n@gmail.com>
2018-05-10 00:05:16 +0500

    Initial commit

---
+++ src/excel_provider.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def normalize(data_frame):
+    return (data_frame - data_frame.mean()) / (data_frame.max() - data_frame.min())  # data_frame.std()
+
+
+def load_data(filename, tablename, columns):
+    data = pd.ExcelFile(filename).parse(tablename)
+    data.datetime = data.datetime.apply(lambda dt: dt.timestamp())
+    y_train = _get_y(data)
+    x_train = _get_x(data, columns)
+    return x_train, y_train
+
+
+def load_x(filename, tablename, columns):
+    data = pd.ExcelFile(filename).parse(tablename)
+    data.datetime = data.datetime.apply(lambda dt: dt.timestamp())
+    x_train = _get_x(data, columns)
+    return x_train
+
+
+def _get_y(data_frame):
+    return data_frame.cnt.values
+
+
+def _get_x(data_frame, columns):
+    table = normalize(data_frame)
+    matrix = table.as_matrix()
+    return matrix[:, :columns]
